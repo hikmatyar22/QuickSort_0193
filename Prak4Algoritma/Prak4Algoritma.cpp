6591ba384eeb81547b04b84be1efbb89678330a1
hikmatyar22 <abiq5161@gmail.com>
2024-03-28 09:27:36 +0700

    Membuat output dan for pada prosedur input

--- Prak4Algoritma/Prak4Algoritma.cpp
+++ Prak4Algoritma/Prak4Algoritma.cpp
@@ -17,4 +17,13 @@ void input() {
 		else
 			cout << "\nMaksimum Panjang Array adalah 20" << endl;
 	}
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
 }
