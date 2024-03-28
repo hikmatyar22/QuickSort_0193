1f953da24091448a02843158013c89f250c48702
hikmatyar22 <abiq5161@gmail.com>
2024-03-28 10:03:30 +0700

    Membuat fungsi main dengan membuat beberapa looping do-while dan kondisi if

--- Prak4Algoritma/Prak4Algoritma.cpp
+++ Prak4Algoritma/Prak4Algoritma.cpp
@@ -88,5 +88,21 @@ void display() {
 	cout << "Number of Data Movements : " << mov_count << endl;
 }
 
+int main() {
+	char ch;
 
+	do {
+		input();
+		q_short(0, n - 1);
+		display();
+		cout << "\n\Do you want to continue? (y/n) : ";
+		cin >> ch;
+		if (ch == 'n' || ch == 'N')
+			break;
+
+		system("pause");
+		system("cls");
+
+	} while (true);
+}
 
